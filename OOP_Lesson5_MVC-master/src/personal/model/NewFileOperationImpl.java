98f6a8240ce2c4126308fe0a66c0f214f697f635
ArinaKozh <arina.mayer66@mail.ru>
2023-04-17 20:55:14 +0300

    1

---
+++ OOP_Lesson5_MVC-master/src/personal/model/NewFileOperationImpl.java
@@ -0,0 +1,59 @@
+package personal.model;
+
+import java.util.List;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewFileOperationImpl extends FileOperationImpl{
+
+    public NewFileOperationImpl(String fileName) {
+        super(fileName);
+    }
+    @Override
+    public void saveAllLines(List<String> lines) {
+            try (FileWriter writer = new FileWriter(fileName, false)) {
+                for (String line : lines) {
+                    // запись всей строки
+                    writer.write(line+"\n");
+                    // запись по символам
+                    writer.append('\n');
+                }
+                writer.flush();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+    }
+    @Override
+    public List<String> readAllLines() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            if (line != null) {
+                lines.add(line);
+                reader.readLine();
+            }
+            while (line != null) {
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                if (line != null) {
+                    lines.add(line);
+                    reader.readLine();
+                }
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+    }
+    
---
+++ OOP_Lesson5_MVC-master/src/personal/model/RepositoryFile.java
@@ -0,0 +1,72 @@
+package personal.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositoryFile implements Repository {
+    private NewUserMapper mapper = new NewUserMapper();
+    private FileOperation fileOperation;
+
+    public RepositoryFile(FileOperation fileOperation) {
+        this.fileOperation = fileOperation;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<String> lines = fileOperation.readAllLines();
+        List<User> users = new ArrayList<>();
+        for (String line : lines) {
+            users.add(mapper.map(line));
+        }
+        return users;
+    }
+
+    @Override
+    public String CreateUser(User user) {
+
+        List<User> users = getAllUsers();
+        int max = 0;
+        for (User item : users) {
+            int id = Integer.parseInt(item.getId());
+            if (max < id) {
+                max = id;
+            }
+        }
+        int newId = max + 1;
+        String id = String.format("%d", newId);
+        user.setId(id);
+        users.add(user);
+        saveUser(users);
+        return id;
+    }
+
+    private void saveUser(List<User> users) {
+        List<String> lines = new ArrayList<>();
+
+        for (User item : users) {
+            lines.add(mapper.map(item));
+        }
+        fileOperation.saveAllLines(lines);
+    }
+
+    public void updateUser(User user) {
+        List<User> users = getAllUsers();
+        for (User item : users) {
+            if (item.getId().equals(user.getId())) {
+                item.setFirstName(user.getFirstName());
+                item.setLastName(user.getLastName());
+                item.setPhone(user.getPhone());
+
+            }
+        }
+        saveUser(users);
+    }
+
+    @Override
+    public void DeleteUser(User user) {
+        List<User> users = getAllUsers();
+        int userId = Integer.parseInt(user.getId());
+        users.remove(userId-1);
+        saveUser(users);
+    }
+}
